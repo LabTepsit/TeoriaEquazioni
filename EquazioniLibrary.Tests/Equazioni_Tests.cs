8281b95c580f6bdc2360ad9625cade60df69d4e0
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-31 10:14:28 +0100

    Test che verifica se l'equazione sia determinata

---
+++ EquazioniLibrary.Tests/Equazioni_Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquazioniLibrary;
+
+namespace EquazioniLibrary.Tests
+{
+    [TestClass]
+    public class Equazioni_Tests
+    {
+        [TestMethod]
+        public void TestIsDetermined()
+        {
+            double a = 3;
+            bool risp_attesa = true;
+            bool risp = Equazioni.IsDetermined(a);
+            Assert.AreEqual(risp_attesa, risp);
+        }
+
+        [TestMethod]
+        public void TestIsDetermined1()
+        {
+            double a = -3;
+            bool risp_attesa = true;
+            bool risp = Equazioni.IsDetermined(a);
+            Assert.AreEqual(risp_attesa, risp);
+        }
+
+        [TestMethod]
+        public void TestIsDetermined2()
+        {
+            double a = 0;
+            bool risp_attesa = false;
+            bool risp = Equazioni.IsDetermined(a);
+            Assert.AreEqual(risp_attesa, risp);
+        }
+    }
+}
---
+++ EquazioniLibrary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EquazioniLibrary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("EquazioniLibrary.Tests")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1cc54d98-ea25-4a15-9eb6-5ff28e8303c7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
